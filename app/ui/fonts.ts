e6fb427af979739a4c9ff522721d56d480b85995
Elbert Jean <elbertjeandossantos@gmail.com>
2024-03-26 12:03:38 -0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Rubik, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const rubik = Rubik({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
